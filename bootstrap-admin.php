d484368a3f6626ea51671063272a6cac0c35bbfd
nerdzgr <info@nerdz.gr>
2012-09-03 18:28:30 +0200

    adding script.js

--- bootstrap-admin.php
+++ bootstrap-admin.php
@@ -11,6 +11,7 @@ Author URI: http://aristeides.com
 
 function bootstrap_admin() {
 	echo '<script src="' .plugins_url('js/bootstrap.min.js', __FILE__). '">';
+	echo '<script src="' .plugins_url('js/script.js', __FILE__). '">';
 	echo '<link rel="stylesheet" type="text/less" href="' .plugins_url('css/style.css', __FILE__). '">';
 }
 
---
+++ js/script.js
@@ -0,0 +1,25 @@
+jQuery(document).ready( function() {
+	jQuery('#adminmenu li a.menu-icon-dashboard').prepend('<i class="icon-home"></i> ');
+	jQuery('#adminmenu li a.menu-icon-post').prepend('<i class="icon-pencil"></i> ');
+	jQuery('#adminmenu li a.menu-icon-page').prepend('<i class="icon-file"></i> ');
+	jQuery('#adminmenu li a.menu-icon-media').prepend('<i class="icon-picture"></i> ');
+	jQuery('#adminmenu li a.menu-icon-links').prepend('<i class="icon-book"></i> ');
+	jQuery('#adminmenu li a.menu-icon-comments').prepend('<i class="icon-comment"></i> ');
+	jQuery('#adminmenu li a.menu-icon-appearance').prepend('<i class="icon-eye-open"></i> ');
+	jQuery('#adminmenu li a.menu-icon-plugins').prepend('<i class="icon-magnet"></i> ');
+	jQuery('#adminmenu li a.menu-icon-users').prepend('<i class="icon-user"></i> ');
+	jQuery('#adminmenu li a.menu-icon-tools').prepend('<i class="icon-wrench"></i> ');
+	jQuery('#adminmenu li a.menu-icon-settings').prepend('<i class="icon-cog"></i> ');
+
+	jQuery('#adminmenu li.wp-not-current-submenu, .folded #adminmenu li.wp-has-submenu').popover({		
+		title: function() {
+			var title = jQuery(this).find('a.menu-top').html();
+			return title;
+		},
+		content: function() {
+			var submenu = jQuery(this).find('.wp-submenu ul').html();
+			return submenu;
+		},
+		placement: 'inside right'
+	});
+});
