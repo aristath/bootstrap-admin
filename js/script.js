ef49fcf28ecdfd360b44f1df791f1541d190dfe5
nerdzgr <info@nerdz.gr>
2012-09-03 19:22:52 +0200

    added more icons

--- js/script.js
+++ js/script.js
@@ -11,6 +11,12 @@ jQuery(document).ready( function() {
 	jQuery('#adminmenu li a.menu-icon-tools').prepend('<i class="icon-wrench"></i> ');
 	jQuery('#adminmenu li a.menu-icon-settings').prepend('<i class="icon-cog"></i> ');
 	jQuery('#adminmenu li a.menu-icon-site').prepend('<i class="icon-th-list"></i> ');
+	jQuery('#adminmenu li a.toplevel_page_psts-checkout').prepend('<i class="icon-heart"></i> ');
+	jQuery('#adminmenu li a.menu-icon-product').prepend('<i class="icon-shopping-cart"></i> ');
+	jQuery('#adminmenu li a.toplevel_page_cp_main').prepend('<i class="icon-filter"></i> ');
+	jQuery('#adminmenu li a.toplevel_page_mp_st').prepend('<i class="icon-signal"></i> ');
+	jQuery('#adminmenu li a.toplevel_page_sitepress-multilingual-cms/menu/languages').prepend('<i class="icon-flag"></i> ');
+	jQuery('#adminmenu li a.toplevel_page_incsub_support').prepend('<i class="icon-fire"></i> ');
 
 	jQuery('#adminmenu li.wp-not-current-submenu, .folded #adminmenu li.wp-has-submenu').popover({		
 		title: function() {
