58427fbc4413b192b8fdb53a42257321271181e9
nerdzgr <info@nerdz.gr>
2012-09-03 18:34:00 +0200

    bugfix

--- bootstrap-admin.php
+++ bootstrap-admin.php
@@ -10,8 +10,8 @@ Author URI: http://aristeides.com
 */
 
 function bootstrap_admin() {
-	echo '<script src="' .plugins_url('js/bootstrap.min.js', __FILE__). '">';
-	echo '<script src="' .plugins_url('js/script.js', __FILE__). '">';
+	echo '<script type="text/javascript" src="' .plugins_url('js/bootstrap.min.js', __FILE__). '"></script>';
+	echo '<script type="text/javascript" src="' .plugins_url('js/script.js', __FILE__). '"></script>';
 	echo '<link rel="stylesheet" type="text/less" href="' .plugins_url('css/style.css', __FILE__). '">';
 }
 
