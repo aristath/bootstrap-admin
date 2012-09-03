e24ec75f22f3dd29e0436cb008d32e58789dfb07
nerdzgr <info@nerdz.gr>
2012-09-03 18:26:00 +0200

    adding bootstrap.min.js

--- bootstrap-admin.php
+++ bootstrap-admin.php
@@ -10,6 +10,7 @@ Author URI: http://aristeides.com
 */
 
 function bootstrap_admin() {
+	echo '<script src="' .plugins_url('js/bootstrap.min.js', __FILE__). '">';
 	echo '<link rel="stylesheet" type="text/less" href="' .plugins_url('css/style.css', __FILE__). '">';
 }
 
