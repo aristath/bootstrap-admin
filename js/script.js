8998c1d1b814e8970fefd2a11b476e15e4c70573
nerdzgr <info@nerdz.gr>
2012-09-05 03:31:03 +0200

    added marketpress hacks for order management pages

--- js/script.js
+++ js/script.js
@@ -30,6 +30,10 @@ jQuery(document).ready( function() {
 	jQuery('#adminmenu li a.toplevel_page_nrelate-main').prepend('<i class="icon-random"></i> ');
 	jQuery('#adminmenu li a.toplevel_page_wdfb').prepend('<i class="icon-share"></i> ');
 	
+	jQuery('tr.mp_order.status-order_received td.mp_orders_status a').append('<div class="progress"><div class="bar bar-danger" style="width: 33%;"></div></div>');
+	jQuery('tr.mp_order.status-order_paid td.mp_orders_status a').append('<div class="progress"><div class="bar bar-danger" style="width: 33%;"></div><div class="bar bar-warning" style="width: 34%;"></div></div>');
+	jQuery('tr.mp_order.status-order_shipped td.mp_orders_status a').append('<div class="progress"><div class="bar bar-danger" style="width: 33%;"></div><div class="bar bar-warning" style="width: 34%;"></div><div class="bar bar-success" style="width: 33%;"></div></div>');
+
 	jQuery('#adminmenu li.wp-not-current-submenu.wp-has-submenu, .folded #adminmenu li.wp-has-submenu').popover({		
 		title: function() {
 			var title = jQuery(this).find('a.menu-top').html();
