4d31621ff913df9d03faf8db595bd6ff6f2fad96
Aristeides Stathopoulos <aristath@gmail.com>
2012-12-04 19:08:10 +0200

    added large font-icons (.icon32) based on http://wordpress.org/support/topic/icon-replacement

--- js/script.js
+++ js/script.js
@@ -1,5 +1,14 @@
 jQuery(document).ready( function() {
-	
+	jQuery('#icon-index.icon32').replaceWith('<div style="font-size:22px;" class="icon32 fonticon"><i class="icon-dashboard icon-large"></i></div> ');
+  jQuery('#icon-edit.icon32-posts-post').replaceWith('<div style="font-size:22px;" class="icon32 fonticon"><i class="icon-th-large icon-large"></i></div> ');
+  jQuery('#icon-upload.icon32').replaceWith('<div style="font-size:22px;" class="icon32 fonticon"><i class="icon-picture icon-large"></i></div> ');
+  jQuery('#icon-link-manager.icon32').replaceWith('<div style="font-size:22px;" class="icon32 fonticon"><i class="icon-link icon-large"></i></div> ');
+  jQuery('#icon-edit-pages.icon32-posts-page').replaceWith('<div style="font-size:22px;" class="icon32 fonticon"><i class="icon-file icon-large"></i></div> ');
+  jQuery('#icon-edit-comments.icon32').replaceWith('<div style="font-size:22px;" class="icon32 fonticon"><i class="icon-comment icon-large"></i></div> ');
+  jQuery('#icon-edit.icon32-posts-portfolio').replaceWith('<div style="font-size:22px;" class="icon32 fonticon"><i class="icon-th-large icon-large"></i></div> ');
+  jQuery('#icon-wpcf7.icon32').replaceWith('<div style="font-size:22px;" class="icon32 fonticon"><i class="icon-edit icon-large"></i></div> ');
+  jQuery('#icon-edit-news.icon32').replaceWith('<div style="font-size:22px;" class="icon32 fonticon"><i class="icon-envelope-alt icon-large"></i></div> ');
+
 	jQuery('tr.mp_order.status-order_received td.mp_orders_status a').append('<div class="progress"><div class="bar bar-danger" style="width: 33%;"></div></div>');
 	jQuery('tr.mp_order.status-order_paid td.mp_orders_status a').append('<div class="progress"><div class="bar bar-danger" style="width: 33%;"></div><div class="bar bar-warning" style="width: 34%;"></div></div>');
 	jQuery('tr.mp_order.status-order_shipped td.mp_orders_status a').append('<div class="progress"><div class="bar bar-danger" style="width: 33%;"></div><div class="bar bar-warning" style="width: 34%;"></div><div class="bar bar-success" style="width: 33%;"></div></div>');
