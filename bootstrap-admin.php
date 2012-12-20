9c5c3c2babe1cb21230a384993096ab7a8b0ac1f
Aristeides Stathopoulos <aristath@gmail.com>
2012-12-20 17:34:25 +0200

    overrides wordpress's wp_default_styles function

--- bootstrap-admin.php
+++ bootstrap-admin.php
@@ -14,7 +14,6 @@ Author URI: http://aristeides.com
 
 require_once 'includes/config.php';
 
-// remove_action( 'wp_default_styles', 'wp_default_styles' );
 function bootstrap_admin_phpless(){
 	require_once( WP_PLUGIN_DIR . '/bootstrap-admin/includes/lessc.inc.php' );
 	lessc::ccompile( WP_PLUGIN_DIR . '/bootstrap-admin/css/global.less', WP_PLUGIN_DIR . '/bootstrap-admin/css/compiled-style.css');
@@ -53,3 +52,61 @@ function bootstrap_admin_scripts(){
 	}
 }
 add_action('admin_enqueue_scripts', 'bootstrap_admin_scripts');
+
+/*
+ * The below is a replacement of the wp_default_styles
+ * function found in wp-includes/script-loader.php.
+ */
+function bootstrap_admin_wp_default_styles( &$styles ) {
+
+  if ( ! $guessurl = site_url() )
+    $guessurl = wp_guess_url();
+
+  $styles->base_url = $guessurl;
+  $styles->content_url = defined('WP_CONTENT_URL')? WP_CONTENT_URL : '';
+  $styles->default_version = get_bloginfo( 'version' );
+  $styles->text_direction = function_exists( 'is_rtl' ) && is_rtl() ? 'rtl' : 'ltr';
+  $styles->default_dirs = array('/wp-admin/', '/wp-includes/css/');
+
+  $suffix = defined('SCRIPT_DEBUG') && SCRIPT_DEBUG ? '' : '.min';
+
+  $rtl_styles = array( 'wp-admin', 'ie', 'media', 'admin-bar', 'customize-controls', 'media-views', 'wp-color-picker' );
+  // Any rtl stylesheets that don't have a .min version
+  $no_suffix = array( 'farbtastic' );
+
+  $styles->add( 'wp-admin', "/wp-admin/css/wp-admin$suffix.css" );
+
+  $styles->add( 'ie', "/wp-admin/css/ie$suffix.css" );
+  $styles->add_data( 'ie', 'conditional', 'lte IE 7' );
+
+  // Register "meta" stylesheet for admin colors. All colors-* style sheets should have the same version string.
+  $styles->add( 'colors', true, array('wp-admin', 'buttons') );
+
+  // do not refer to these directly, the right one is queued by the above "meta" colors handle
+  // $styles->add( 'colors-fresh', "/wp-admin/css/colors-fresh$suffix.css", array('wp-admin', 'buttons') );
+  // $styles->add( 'colors-classic', "/wp-admin/css/colors-classic$suffix.css", array('wp-admin', 'buttons') );
+
+  $styles->add( 'media', "/wp-admin/css/media$suffix.css" );
+  $styles->add( 'install', "/wp-admin/css/install$suffix.css", array('buttons') );
+  $styles->add( 'thickbox', '/wp-includes/js/thickbox/thickbox.css', array(), '20121105' );
+  $styles->add( 'farbtastic', '/wp-admin/css/farbtastic.css', array(), '1.3u1' );
+  $styles->add( 'wp-color-picker', "/wp-admin/css/color-picker$suffix.css" );
+  $styles->add( 'jcrop', "/wp-includes/js/jcrop/jquery.Jcrop.min.css", array(), '0.9.10' );
+  $styles->add( 'imgareaselect', '/wp-includes/js/imgareaselect/imgareaselect.css', array(), '0.9.8' );
+  $styles->add( 'admin-bar', "/wp-includes/css/admin-bar$suffix.css" );
+  $styles->add( 'wp-jquery-ui-dialog', "/wp-includes/css/jquery-ui-dialog$suffix.css" );
+  $styles->add( 'editor-buttons', "/wp-includes/css/editor$suffix.css" );
+  $styles->add( 'wp-pointer', "/wp-includes/css/wp-pointer$suffix.css" );
+  $styles->add( 'customize-controls', "/wp-admin/css/customize-controls$suffix.css", array( 'wp-admin', 'colors', 'ie' ) );
+  $styles->add( 'media-views', "/wp-includes/css/media-views$suffix.css", array( 'buttons' ) );
+  // $styles->add( 'buttons', "/wp-includes/css/buttons$suffix.css" );
+  $styles->add( 'buttons', plugins_url( 'css/buttons.css', __FILE__ ) );
+
+  foreach ( $rtl_styles as $rtl_style ) {
+    $styles->add_data( $rtl_style, 'rtl', true );
+    if ( $suffix && ! in_array( $rtl_style, $no_suffix ) )
+      $styles->add_data( $rtl_style, 'suffix', $suffix );
+  }
+}
+remove_action( 'wp_default_styles', 'wp_default_styles' );              // removes the default wp_default_styles function
+add_action( 'wp_default_styles', 'bootstrap_admin_wp_default_styles' ); // adds our customized bootstrap_admin_wp_default_styles function
