7f7eeae8c533cb38cceee99328f62e91d38f56f4
Aristeides Stathopoulos <aristath@gmail.com>
2012-12-27 11:19:00 +0200

    Replacing Font-Awesome with the Elusive-Icons font

--- includes/config.php
+++ includes/config.php
@@ -1,7 +1,7 @@
 <?php
 
 // Set to 1 to use php-less compiling. Useful while theming using less
-define('BOOTSTRAP_ADMIN_LESS_MODE', '0');
+define('BOOTSTRAP_ADMIN_LESS_MODE', '1');
 
 // Set to 1 to use chosen js for select elements
 define('BOOTSTRAP_ADMIN_CHOSEN_JS', '0');
